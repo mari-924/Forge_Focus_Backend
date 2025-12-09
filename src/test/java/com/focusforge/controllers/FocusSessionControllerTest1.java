62463f028858264a66429b9957e6ec7474c60ea3
MatthijsDV2004 <mdevries@csumb.edu>
2025-12-09 12:01:00 -0800

    Update FocusSessionControllerTest1.java

--- src/test/java/com/focusforge/controllers/FocusSessionControllerTest1.java
+++ src/test/java/com/focusforge/controllers/FocusSessionControllerTest1.java
@@ -18,7 +18,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-public class FocusSessionControllerTest {
+public class FocusSessionControllerTest1 {
 
     @Mock
     private FocusSessionRepository sessionRepository;
